a6823f9a980641d268db3004ec345468e30e7760
Rabi Gautam <rabigautam43@gmail.com>
2021-07-11 17:53:26 +0545

    shuffle

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
-// import "fmt"
+import "fmt"
 
 
 func main() {
 	cards := newDeck() //slice of type string
-	// cards.print()
-	// handDeck,remaningDeck:=deal(cards,10)
-	// handDeck.print()
-	// fmt.Println("above is handCard")
-	// remaningDeck.print()	
-	// fmt.Println(cards.toString())
-	// cards.saveToFile("test go file")
-	// readCards:=newDeckFromFile("test go file")
-	// readCards.print()
+	cards.print()
+	handDeck,remaningDeck:=deal(cards,10)
+	handDeck.print()
+	fmt.Println("above is handCard")
+	remaningDeck.print()	
+	fmt.Println(cards.toString())
+	cards.saveToFile("test go file")
+	readCards:=newDeckFromFile("test go file")
+	readCards.print()
+	fmt.Println("Shuffle start...")
+
 	cards.shuffle()
 	cards.print()
 }
