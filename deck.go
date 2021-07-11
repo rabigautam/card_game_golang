2878bfee56e9869e1ddd5bfb0fb3b81112b71438
Rabi Gautam <rabigautam43@gmail.com>
2021-07-11 11:23:09 +0545

    added functionality with newDeck and reciver function

---
+++ deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//Create new type of deck
+//slice of stringd
+
+type deck []string
+
+func newDeck() deck {				//this function doesn't require receiver as it doesn't call method name
+	cards:=deck{}
+	cardsSuits:= []string{"Club","Spades","Hearts","Diamonds"}
+	cardsValues:= []string{"Ace","Two","Three","Four"}
+
+	for _,suit:=range cardsSuits{
+		for _, value:=range cardsValues{
+			cards=append(cards, value+"of "+suit)
+		}
+	}
+	return cards
+}
+
+func (cards deck) print(){   //receiver function 
+	for i,card:=range cards{
+		fmt.Println(i,card)
+
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	cards := newDeck() //slice of type string
+	cards.print()
+}
+
