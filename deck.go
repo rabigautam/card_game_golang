d06fb3ccbbcd42b16041571997597c789e396cb6
Rabi Gautam <rabigautam43@gmail.com>
2021-07-11 14:21:34 +0545

    added multiple return function with slice range

--- deck.go
+++ deck.go
@@ -7,22 +7,25 @@ import "fmt"
 
 type deck []string
 
-func newDeck() deck {				//this function doesn't require receiver as it doesn't call method name
-	cards:=deck{}
-	cardsSuits:= []string{"Club","Spades","Hearts","Diamonds"}
-	cardsValues:= []string{"Ace","Two","Three","Four"}
+func newDeck() deck { //this function doesn't require receiver as it doesn't call method name
+	cards := deck{}
+	cardsSuits := []string{"Club", "Spades", "Hearts", "Diamonds"}
+	cardsValues := []string{"Ace", "Two", "Three", "Four"}
 
-	for _,suit:=range cardsSuits{
-		for _, value:=range cardsValues{
-			cards=append(cards, value+"of "+suit)
+	for _, suit := range cardsSuits {
+		for _, value := range cardsValues {
+			cards = append(cards, value+"of "+suit)
 		}
 	}
 	return cards
 }
 
-func (cards deck) print(){   //receiver function 
-	for i,card:=range cards{
-		fmt.Println(i,card)
+func (cards deck) print() { //receiver function
+	for i, card := range cards {
+		fmt.Println(i, card)
 
 	}
+}
+func deal(cards deck, handSize int) (deck, deck) {               //multiple return
+	return cards[:handSize],cards[handSize:]
 }
--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
+import "fmt"
+
+
 func main() {
 	cards := newDeck() //slice of type string
 	cards.print()
+	handDeck,remaningDeck:=deal(cards,10)
+	handDeck.print()
+	fmt.Println("above is handCard")
+	remaningDeck.print()	
 }
 
